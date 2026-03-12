37731df42fa0c90e75d920b6072cba5b1f43a192
Isha <isha@Ishas-Mac-mini.local>
2026-03-12 16:42:28 +0530

    first commit

---
+++ color.php
@@ -0,0 +1,67 @@
+
+
+
+/*** 
+=====================================================
+	Theme Main Color Css
+=====================================================
+***/
+
+/* template-color */
+
+
+
+
+:root {
+    --theme-color:#f59e0b!important;
+}
+
+
+
+/*** 
+=====================================================
+	Theme Main Background Color Css
+=====================================================
+***/
+
+
+
+
+{
+	background: #f59e0b!important;
+	background-color:#f59e0b!important;
+}
+
+
+/************** border-color***************/
+
+
+
+{
+	border-color:#f59e0b!important;
+}
+
+/* ------------------////End of Section////--------------*/
+
+/************** box-shadow***************/
+.featured-three-column .column .inner-box:hover
+
+/************** box-shadow***************/
+{
+	box-shadow:0px 0px 10px rgba(245,158,11,0.8) !important;
+}
+
+/* ------------------////End of Section////--------------*/
+
+/**************  border-color***************/
+
+.latest_blog_wrapper .post .date:after,.blog_post_meta .post .date:after
+
+/**************  border-color rgba***************/
+{
+    border-color: rgba(253, 199, 22, 0)!important;
+}
+/* ------------------////End of Section////--------------*/
+
+
+
